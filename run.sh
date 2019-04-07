079516aec7a21642a9b93451ee5da3bdb220d7f4
García Arevalo <gpablo@netapp.com>
2019-04-07 22:13:53 +0200

    version 2.3

--- run.sh
+++ run.sh
@@ -34,10 +34,3 @@ kubectl create -f ./02_k8s_example/sc-nas-silver.yaml
 kubectl patch storageclass san -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
-chmod u+x install-helm.sh
-./install-helm.sh
-
-kubectl -n kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
