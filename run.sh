4e8503041aba664204428c79e672bc285fba3ef1
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2019-10-30 20:10:42 +0100

    version 2.0

--- run.sh
+++ run.sh
@@ -12,17 +12,23 @@ EOT
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-ansible-playbook ./01_start/configure_ontap_svm_for_iscsi.yml
-ansible-playbook ./01_start/connect_iscsi_initiators.yml
-
 kubectl delete sc storage-class-nas
 kubectl delete sc storage-class-san
 kubectl delete sc storage-class-solidfire
 kubectl delete sc storage-class-ssd
 kubectl delete sc storage-class-storagepool
+kubectl delete sc gold
+kubectl delete sc silver
+kubectl delete sc solidfire-bronze-two
+kubectl delete sc solidfire-gold-four
+kubectl delete sc solidfire-silver
+kubectl delete sc solidfire-silver-one 
+kubectl delete sc solidfire-silver-three
+
 
 tridentctl delete backend BackendForNAS -n trident
 tridentctl delete backend solidfire_192.168.0.130 -n trident
+tridentctl delete backend BackendForSolidFire -n trident
 
 tridentctl create backend --filename ./02_k8s_example/backend-nas.json -n trident
 tridentctl create backend --filename ./02_k8s_example/backend-san.json -n trident
@@ -30,6 +36,8 @@ tridentctl create backend --filename ./02_k8s_example/backend-san.json -n triden
 kubectl create -f ./02_k8s_example/sc-san.yaml
 kubectl create -f ./02_k8s_example/sc-nas-gold.yaml
 kubectl create -f ./02_k8s_example/sc-nas-silver.yaml
+
+kubectl create -f ./02_k8s_example/snap-sc.yaml
 
 kubectl patch storageclass san -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
