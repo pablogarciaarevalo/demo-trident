9e8a935e4ad7f2728687d4c29ce4528425642993
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2019-11-07 22:06:01 +0100

    simulate oc cli

--- run.sh
+++ run.sh
@@ -36,3 +36,5 @@ kubectl create -f ./k8s_files/snap-sc.yaml
 
 kubectl patch storageclass san -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
+# Simulate OpenShift cli
+alias oc=kubectl
