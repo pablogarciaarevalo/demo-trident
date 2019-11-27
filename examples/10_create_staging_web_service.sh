9a1ebbbbdc3ac66302ebb784f955c1e153ee53c0
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2019-11-27 22:43:11 +0100

    improve clones

--- examples/10_create_staging_web_service.sh
+++ examples/10_create_staging_web_service.sh
@@ -1,8 +1,25 @@
-
-cat web-content-v2.yaml 
-kubectl apply -f web-content-v2.yaml -n web
-
 clear
+echo "[root@rhel3 ~]# cat ../k8s_files/web-content-v2.yaml"
+cat ../k8s_files/web-content-v2.yaml 
+
+echo ""
+read -p "Press any key to continue... " -n1 -s
+clear
+
+echo "[root@rhel3 ~]# kubectl apply -f ../k8s_files/web-content-v2.yaml -n web"
+kubectl apply -f ../k8s_files/web-content-v2.yaml -n web
+
+echo ""
+read -p "Press any key to continue... " -n1 -s
+clear
+
+echo "[root@rhel3 ~]# cat ../k8s_files/web-v1.yml"
+cat ../k8s_files/web-v1.yml 
+
+echo ""
+read -p "Press any key to continue... " -n1 -s
+clear
+
 echo "[root@rhel3 ~]# kubectl apply -f ../k8s_files/web-v1.yml -n web"
 kubectl apply -f ../k8s_files/web-v1.yml -n web
 echo "[root@rhel3 ~]# kubectl get all -n web"
