8a6386884f8c3f1b561efd5b30fc76a294752132
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2019-11-28 12:38:57 +0100

    3 pods for manual scalying

--- examples/06_create_frontend_service.sh
+++ examples/06_create_frontend_service.sh
@@ -1,8 +1,8 @@
 clear
 echo "[root@rhel3 ~]# cat ../k8s_files/frontend.yaml"
 cat ../k8s_files/frontend.yaml
-recho ""
-ead -p "Press any key to continue... " -n1 -s
+echo ""
+read -p "Press any key to continue... " -n1 -s
 clear
 echo "[root@rhel3 ~]# kubectl create -f ../k8s_files/frontend.yaml"
 kubectl create -f ../k8s_files/frontend.yaml
