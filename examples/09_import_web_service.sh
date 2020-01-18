a116319fa876fb51f289a97588e3b5b5df4f6f31
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-01-18 19:18:21 +0100

    remove  no-manage flag

--- examples/09_import_web_service.sh
+++ examples/09_import_web_service.sh
@@ -7,8 +7,8 @@ cat ../k8s_files/web-content-v1.yaml
 read -p "Press any key to continue... " -n1 -s
 clear
 
-echo "[root@rhel3 ~]# tridentctl import volume BackendForNAS web_content -f ../k8s_files/web-content-v1.yaml --no-manage -n trident"
-tridentctl import volume BackendForNAS web_content -f ../k8s_files/web-content-v1.yaml --no-manage -n trident
+echo "[root@rhel3 ~]# tridentctl import volume BackendForNAS web_content -f ../k8s_files/web-content-v1.yaml -n trident"
+tridentctl import volume BackendForNAS web_content -f ../k8s_files/web-content-v1.yaml -n trident
 echo ""
 echo "[root@rhel3 ~]# kubectl get pvc -n web"
 kubectl get pvc -n web
