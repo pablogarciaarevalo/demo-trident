d49486ccb030097a6e93d2a815385a2dc47a37f8
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-03-23 16:13:02 +0100

    Enable the K8s snapshots after install Trident 20.01

--- demo/02_install_trident.sh
+++ demo/02_install_trident.sh
@@ -11,3 +11,19 @@ echo "[root@rhel3 ~]# ./tridentctl obliviate alpha-snapshot-crd"
 ./tridentctl obliviate alpha-snapshot-crd
 echo "[root@rhel3 ~]# ./tridentctl install -n trident"
 ./tridentctl install -n trident
+
+echo "Enabling snapshots for K8s 1.17 or later"
+
+# Install Snapshot Beta CRDs per cluster
+echo "[root@rhel3 ~]# kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml"
+kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+echo "[root@rhel3 ~]# kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml"
+kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+echo "[root@rhel3 ~]# kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml"
+kubectl create -f  https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+
+# Install Snapshot Controller per cluster
+echo "[root@rhel3 ~]# kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+echo "[root@rhel3 ~]# kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/release-2.0/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
