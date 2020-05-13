6b2a8caf3cf1095ccb873b605ba20de9701a7a27
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-05-13 08:37:17 +0200

    Run twice tridentctl obliviate alpha-snapshot-crd
    https://github.com/NetApp/trident/issues/394

--- demo/02_install_trident.sh
+++ demo/02_install_trident.sh
@@ -10,7 +10,9 @@ cd trident-installer
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/demo-trident/demo/trident-installer:/root/bin
 
+# Run twice the obliviate alpha-snapshot-crd command due to a known issue
 echo "[root@rhel3 ~]# tridentctl obliviate alpha-snapshot-crd"
+tridentctl obliviate alpha-snapshot-crd
 tridentctl obliviate alpha-snapshot-crd
 
 # Installing Trident using the operator
