b8e4d5ffb9ec88b05465c34b0e78e8f95a0aab7e
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-06-22 16:07:23 +0200

    new apps

--- demo/07_todo_app_3tier.sh
+++ demo/07_todo_app_3tier.sh
@@ -8,8 +8,6 @@ read -p "Press any key to continue... " -n1 -s
 clear
 echo "[root@rhel3 ~]# kubectl apply -f k8s_files/todo-app-3tier.yaml"
 kubectl apply -f k8s_files/todo-app-3tier.yaml
-echo "[root@rhel3 ~]# kubectl get service"
-kubectl get service
+echo "[root@rhel3 ~]# kubectl get all -n todo-app"
+kubectl get all -n todo-app
 echo ""
-echo "[root@rhel3 ~]# kubectl get pods"
-kubectl get pods
